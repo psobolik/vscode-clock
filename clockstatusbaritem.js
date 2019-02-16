d231eae30bff3e4ad45fb3fdb1e99a802faf86e2
Paul Sobolik <psobolik@live.com>
2019-02-16 11:20:45 -0500

    Fixed configurable interval implementation

--- clockstatusbaritem.js
+++ clockstatusbaritem.js
@@ -15,14 +15,18 @@ class StatusBarItem {
     this._statusBarItem.tooltip = 'Click to insert into selection';
     this._statusBarItem.show();
 
-    this._interval = setInterval(() => this.refreshUI(), 1000);
-
     this._configurationChangeListener = vscode.workspace.onDidChangeConfiguration(((event) => {
       this.configurationChangeHandler(event);
     }).bind(this));
 
+    this.setClockInterval();
     this.setClockColor();
     this.refreshUI();
+  }
+
+  setClockInterval() {
+    clearInterval(this._interval);
+    this._interval = setInterval(() => this.refreshUI(), vscode.workspace.getConfiguration('clock').clockInterval || DEFAULT_CLOCK_INTERVAL);
   }
 
   setClockColor() {
@@ -33,17 +37,19 @@ class StatusBarItem {
     if (event.affectsConfiguration('clock.color')) {
       this.setClockColor();
     }
+    if (event.affectsConfiguration('clock.clockInterval')) {
+      this.setClockInterval();
+    }
   }
 
   dispose() {
     this._configurationChangeListener.dispose();
     this._statusBarItem.dispose();
-    clearTimeout(this._timeout);
+    clearInterval(this._interval);
   }
 
   refreshUI() {
     this._statusBarItem.text = clockService();
-    this._timeout = setTimeout(() => this.refreshUI(), vscode.workspace.getConfiguration('clock').clockInterval || DEFAULT_CLOCK_INTERVAL);
   }
 }
 
